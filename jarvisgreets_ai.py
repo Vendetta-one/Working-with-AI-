70f465a9a74cd6e3766e7fcd297fbe55b3931e53
Vendetta-one <pratapranavansh@gmail.com>
2026-05-29 03:50:41 +0530

    used match/case to define spl cases, also improved efficiency

--- jarvisgreets_ai.py
+++ jarvisgreets_ai.py
@@ -1,30 +1,40 @@
 from google import genai
-
-#a function that handles the api call
-def generate_response(prompt):
-    jarvis = genai.Client(api_key="AIzaSyAMnBZCclJZE3-skqyLBJK4z1I8NsqwCeE")
-
-    if prompt.lower() == "quit":
-     print("Goodbye!")
-     exit()
-    else:
-        response = jarvis.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
-    )
-    print("Jarvis:", response.text)
-    print()
+jarvis = genai.Client(api_key="AIzaSyDuOUgSwTLWqXxjdnAyFbxQAsS8laXmuO8")
+    
 
 #a function that greets the user with their name 
 def greeting():
     name = input("what is your name?: ").strip().title()
     print(f"Hello, {name}!")
 
+# checking for keywords in the prompt and responding accordingly
+def checkfor_keys(prompt):
+    match prompt.lower():
+        case "quit":
+            print("Goodbye!")
+            exit()
+        case "help":
+            print("You can ask me anything! Just type your question and I'll do my best to help you. If you want to exit, just type 'quit'.")
+           
+        case "clear":
+            print("\n "*100)
+           
 
-def main():
-    greeting()
+        case _:
+
+            response = jarvis.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+            )
+            print("Jarvis:", response.text)
+        
+
+        
+def ask_for_prompt():
     while True:
         prompt = input("your prompt: ").strip()
-        generate_response(prompt)
+        checkfor_keys(prompt)
+        
+greeting()
+ask_for_prompt()
 
-main()
